f589a8658f165a68ccda924862d070431579e8b3
brandonn <brandon7.7porcel@gmail.com>
2021-11-14 18:08:21 -0300

    diseno cuasi listo

--- vistas/componentes/header.php
+++ vistas/componentes/header.php
@@ -4,6 +4,6 @@
             </h1>
 			<div class="sign-buttons">
 				<a href="./vistas/login.php">Ingresar</a>
-				<a href="./vistas/reguster.php">Registrarme</a>
+				<a href="./vistas/register.php">Registrarme</a>
 			</div>
 </header>
