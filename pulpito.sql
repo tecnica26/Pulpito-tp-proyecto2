256ba1f9c4a7abb6ff5c02b70c056d8a26c26fbb
brandonn <brandon7.7porcel@gmail.com>
2021-11-22 11:40:30 -0300

    act bdatos

--- pulpito.sql
+++ pulpito.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-11-2021 a las 14:36:17
+-- Tiempo de generación: 22-11-2021 a las 15:39:33
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 8.0.12
 
@@ -31,9 +31,16 @@ CREATE TABLE `obras` (
   `id` int(11) NOT NULL,
   `nombre` varchar(30) NOT NULL,
   `img_link` text NOT NULL,
-  `descripcion` varchar(50) NOT NULL,
+  `descripcion` text NOT NULL,
   `link` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `obras`
+--
+
+INSERT INTO `obras` (`id`, `nombre`, `img_link`, `descripcion`, `link`) VALUES
+(1, 'Happy Together', 'https://variety.com/wp-content/uploads/2017/10/rexfeatures_5867685c.jpg', 'Película sobre una pareja gay que llega a Argentina desde Hong Kong buscando una mejor vida. Dirigida por Wong Kar-wai.', 'https://www.youtube.com/watch?v=5VPvFaAWX9U');
 
 -- --------------------------------------------------------
 
@@ -52,7 +59,12 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nombre_usuario`, `contrasenia`) VALUES
-(16, 'nike', '41fd220f05ed0d8c56e3b83af87d45d7');
+(16, 'nike', '41fd220f05ed0d8c56e3b83af87d45d7'),
+(17, 'adidas', '28a34010e84b881fb087359c7e280a08'),
+(18, 'puma', 'd9f32436125b47e03d11fbf1fa62424a'),
+(19, 'kappa', 'fe1480ff8b6ed22e7723cda0145ef23d'),
+(20, 'goku', 'bef27466a245ce3ec692bd25409c2549'),
+(21, 'brandon', 'fc275ac3498d6ab0f0b4389f8e94422c');
 
 --
 -- Índices para tablas volcadas
@@ -78,13 +90,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `obras`
 --
 ALTER TABLE `obras`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
