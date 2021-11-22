2119414fcacb04faae0611d957eff5ced7f7ab23
brandonn <brandon7.7porcel@gmail.com>
2021-11-22 11:37:29 -0300

    listado de recs. listo,proyecto cuasi terminado

--- index.php
+++ index.php
@@ -13,10 +13,6 @@ session_start();
 <body>
 	<div class="container">
 
-
-
-
-
 <header class="header">
 			<h1>
                 <a href="index.php">PULPITO</a>    
@@ -37,82 +33,9 @@ else{
 ?>
 			</div>
 </header>
+<div class="content-container">
+<?php
 
-		
-		<div class="content-container">
-		<!-- if estas logueado,nostrar -->
-		<div class="recos-container">
-
-			<article class="rec-card">
-				<div class="rec-img-container">
-					<a href="">
-						<img class="rec-img" src="https://contentv2.tap-commerce.com/cover/large/9789878317090_1.jpg?id_com=1113" alt="SOBRE HÉROES Y TUMBAS  V1 image">
-					</a>
-				</div>
-				<div class="rec-body">
-					<div class="rec-title">
-						<b title="SOBRE HÉROES Y TUMBAS  V1" class="rec-name">
-							SOBRE HÉROES Y TUMBAS  V1
-						</b>
-					</div>
-					<div class="rec-description">
-						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio, perspiciatis.
-					</div>
-					<div class="rec-ver-mas">
-						<a href="">Ver más</a>
-					</div>
-				</div>
-			</article>
-			<article class="rec-card">
-				<div class="rec-img-container">
-					<a href="">
-						<img class="rec-img" src="https://contentv2.tap-commerce.com/cover/large/9789878317090_1.jpg?id_com=1113" alt="SOBRE HÉROES Y TUMBAS  V1 image">
-					</a>
-				</div>
-				<div class="rec-body">
-					<div class="rec-title">
-						<b title="SOBRE HÉROES Y TUMBAS  V1" class="rec-name">
-							SOBRE HÉROES Y TUMBAS  V1
-						</b>
-					</div>
-					<div class="rec-description">
-						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio, perspiciatis.
-					</div>
-				
-					<div class="rec-ver-mas">
-						<a href="">Ver más</a>
-					</div>
-				</div>
-			</article>
-			<article class="rec-card">
-				<div class="rec-img-container">
-					<a href="">
-						<img class="rec-img" src="https://contentv2.tap-commerce.com/cover/large/9789878317090_1.jpg?id_com=1113" alt="SOBRE HÉROES Y TUMBAS  V1 image">
-					</a>
-				</div>
-				<div class="rec-body">
-					<div class="rec-title">
-						<b title="SOBRE HÉROES Y TUMBAS  V1" class="rec-name">
-							SOBRE HÉROES Y TUMBAS  V1
-						</b>
-					</div>
-					<div class="rec-description">
-						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio, perspiciatis.
-					</div>
-					<div class="rec-ver-mas">
-						<a href="">Ver más</a>
-					</div>
-				</div>
-			</article>
-		
-		</div>
-			<table border="1">
-				<tr>
-					<td>id</td>
-					<td>descripcion</td>
-					<td>linkvermas</td>
-				</tr>
-				<?php
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -122,28 +45,49 @@ $dbname = "pulpito";
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, nombre FROM obras";
+$sql = "SELECT id, nombre, img_link,descripcion,link FROM obras";
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "<br> id: ". $row["id"]. " - Name: ". $row["nombre"]. " <br>";
-    }
-} else {
-    echo "0 results";
-}
-
-$conn->close();
-?>
-			</table>
-			<!-- if no estas login,,,no mostrar -->
-				<!-- <p>no estas logueado amigue</p>
-				<a href="./vistas/login.php">logueate perro anashex</a> -->
-			</div>
+		if(isset($_SESSION['usuario'])){
+			if ($result->num_rows > 0) {
+				while($row = $result->fetch_assoc()) {
+					echo "	
+						<div class='recos-container'>
+							<article class='rec-card'>
+								<div class='rec-img-container'>
+									<a href=''>
+										<img class='rec-img' src='" . $row["img_link"]. "'>
+									</a>
+								</div>
+								<div class='rec-body'>
+									<div class='rec-title'>
+										<b title='SOBRE HÉROES Y TUMBAS  V1' class='rec-name'>
+										" . $row["nombre"]. "
+										</b>
+									</div>
+									<div class='rec-description'>
+									" . $row["descripcion"]. "
+									</div>
+									<div class='rec-ver-mas'>
+										<a href='" . $row["link"]. "'>Ver más</a>
+									</div>
+								</div>
+							</article>			
+						</div>			
+					";
+				}
+			} else {
+				echo "0 results";
+			}
+			$conn->close();
+		}
+		else{
+			echo "No estas logueade amigue";
+		}
+		?>
 		<footer class="footer">
 				<a href="./vistas/acerca.html">Quienes Somos?</a>
 				| 
